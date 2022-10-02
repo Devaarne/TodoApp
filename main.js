c97ce4313064e2c8c10eb285aef56bb2714c0b69
Dev Muller <50141214+Devaarne@users.noreply.github.com>
2022-10-03 00:34:19 +0200

    updates to code
    improved comments across css html and js ; improved indentation, added possible code method for sorting list alphabetically, however not functional

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.addEventListener('load',()=>{
 
     nameInput.value = username;
 
-    nameInput.addEventListener('change', e => {
+    nameInput.addEventListener('change', (e) => {
         localStorage.setItem('username', e.target.value);
     })
 
@@ -29,34 +29,6 @@ window.addEventListener('load',()=>{
 
         DisplayTodos();
 
-        const todos = [];  
-
-const doSort = (todos) => {
-    return todos
-         .sort((a, b) => {
-
-            if (a.value > b.value) { return 1; }
-            if (a.value > b.value) { retrun -1; }
-            return 0;
-         })
-}
-
-document.querySelector ('#Add').addEventListener('click', () => {
-
-    const data = document.querySelector('#myInput');
-    todos.push(data.value);
-
-    var mapped = todos.map(
-        (el, i) => ({ index: i, value: el.toLowerCase() })
-    );
-
-    const sortedTodos = doSort (mapped)
-         .map(el => todos[el.index]);
-
-    todoList.innerHTML = sortedTodos.map(todo => '<li>' + todo + '</li>').join('');
-
-    data.value = '';
-
 
 
     })
@@ -66,12 +38,11 @@ document.querySelector ('#Add').addEventListener('click', () => {
 
 function DisplayTodos () {
     const todoList = document.querySelector('#todo-list');
-
     todoList.innerHTML = '';
 
     todos.forEach(todo => {
         const todoItem = document.createElement('div');
-        todoItem.classList.add('todo-item')
+        todoItem.classList.add('todo-item');
 
         const label = document.createElement('label');
         const input = document.createElement('input');
@@ -114,7 +85,7 @@ function DisplayTodos () {
             todoItem.classList.add('done');
         }
 
-        input.addEventListener('click', e => {
+        input.addEventListener('click', (e) => {
             todo.done = e.target.checked;
             localStorage.setItem('todos', JSON.stringify(todos));
 
@@ -128,19 +99,19 @@ function DisplayTodos () {
 
         })
 
-        edit.addEventListener('click', e => {
+        edit.addEventListener('click', (e) => {
             const input = content.querySelector('input');
             input.removeAttribute('readonly');
             input.focus();
-            input.addEventListener('blur', e => {
+            input.addEventListener('blur', (e) => {
                 input.setAttribute('readonly', true);
                 todo.content = e.target.value;
                 localStorage.setItem('todos', JSON.stringify(todos));
-                DisplayTodos();
+                DisplayTodos()
             })
         } )
 
-        deleteButton.addEventListener('click', e => {
+        deleteButton.addEventListener('click', (e) => {
             todos = todos.filter(t => t != todo);
             localStorage.setItem('todos', JSON.stringify(todos));
             DisplayTodos();
